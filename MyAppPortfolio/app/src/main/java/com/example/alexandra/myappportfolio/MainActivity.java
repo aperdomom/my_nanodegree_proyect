fa84763fd7cb7834e9121c2c7da99c5df0743b47
Alexandra Perdomo <aperdomo@mahisoft.com>
2015-09-24 22:40:24 -0600

    first commit

---
+++ MyAppPortfolio/app/src/main/java/com/example/alexandra/myappportfolio/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.alexandra.myappportfolio;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void sendMessage(View view) {
+        String toastMsg = getString(R.string.msg_toast_apps)
+                +" " +
+                ((Button) view).getText().toString().toLowerCase()
+                +"!";
+        Toast.makeText(this,toastMsg,Toast.LENGTH_LONG).show();
+    }
+}
